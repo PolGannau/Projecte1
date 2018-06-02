146f63dedf9711c5c4327915522d4e22f54ca37a
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-06-02 20:04:52 +0200

    Preparing path

--- --AndroDunos--#GAME_FILES#/Enemy_StraightOnShip.cpp
+++ --AndroDunos--#GAME_FILES#/Enemy_StraightOnShip.cpp
@@ -44,9 +44,32 @@ Enemy_StraightOnShip::Enemy_StraightOnShip(int x, int y, int num) : Enemy(x, y)
 		path.PushBack({ -2.0f,0.0f }, 37); 
 		path.PushBack({ 2.0f,0.8f }, 40); 
 		path.PushBack({ -4.0f,0.0f }, 500);
-		movement = 3;
+		movement = 1;
 	}
+	else if (num == 4) { 
+		fly.PushBack({ 187,1,29,13 });
+		fly.PushBack({ 217, 0, 29, 16 });
+		fly.PushBack({ 247, 1, 29, 17 });
+		fly.PushBack({ 217, 0, 29, 16 });
+		fly.PushBack({ 277, 1, 29, 14 });
+		fly.speed = 0.1f;
+		animation = &fly;
+		// POSA AQUI EL PATH DEL NUMERO 4
+		
+		movement = 1;
+	}
+	else if (num == 5) { 
+		fly.PushBack({ 187,1,29,13 });
+		fly.PushBack({ 217, 0, 29, 16 });
+		fly.PushBack({ 247, 1, 29, 17 });
+		fly.PushBack({ 217, 0, 29, 16 });
+		fly.PushBack({ 277, 1, 29, 14 });
+		fly.speed = 0.1f;
+		animation = &fly;
+		// POSA AQUI EL PATH DEL NUMERO 5
 
+		movement = 1;
+	}
 
 	collider = App->collision->AddCollider({ 0, 0, 29, 16 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 	original_pos.x = x;
@@ -56,7 +79,7 @@ Enemy_StraightOnShip::Enemy_StraightOnShip(int x, int y, int num) : Enemy(x, y)
 void Enemy_StraightOnShip::Move()
 {
 	
-	if (movement==1 || movement==3) {
+	if (movement == 1) {
 		position = original_pos + path.GetCurrentSpeed();
 	}
 	else if (movement == 2) {
